45089e7f4f530b23a7ad5fd78e6d96ae7df81e35
Nisha <nisha@Nishas-MacBook-Pro.local>
2020-12-10 22:40:03 -0500

    Add the header component for the page

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+
+function header({title}) {
+    let classname = 'ownUp'
+
+    return (
+        <Jumbotron>
+            <h1  className={`${classname}-Header`}> {title} </h1>
+        </Jumbotron>
+    )
+}
+
+export default header
